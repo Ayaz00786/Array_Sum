ec45c9ced22f960ada8fe838a349ccda010712df
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-26 11:00:43 +0530

    Add project files.

---
+++ Array_Sum/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array_Sum
+{
+    internal class Class1
+    {
+        public static void Array()
+        {
+            int[] array = new int[10];
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter number in Position {0}:", i);
+                int num = int.Parse(Console.ReadLine());
+                array[i] = num;
+                sum = sum + array[i];
+            }
+            Console.WriteLine("\nSum Of Total Number Is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Array_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Array();
+        }
+    }
+}
